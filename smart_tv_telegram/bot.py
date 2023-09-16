a2663d1b399d563a22a998ec890602749c4e8346
Zhongke Chen <ch3n2k@gmail.com>
2023-09-16 13:29:01 -0700

    add url download function
    add ffmpeg to dockerfile
    
    fix health check
    
    add exceptions when download failed

--- smart_tv_telegram/bot.py
+++ smart_tv_telegram/bot.py
@@ -1,9 +1,14 @@
 import abc
+import asyncio
 import enum
 import functools
 import html
+import os
+import os.path
+import re
 import traceback
 import typing
+import tempfile
 
 import async_timeout
 from pyrogram import Client, filters
@@ -22,11 +27,13 @@ __all__ = [
 
 _REMOVE_KEYBOARD = ReplyKeyboardRemove()
 _CANCEL_BUTTON = "^Cancel"
+_URL_PATTERN = r'(http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])'
 
 
 class States(enum.Enum):
     NOTHING = enum.auto()
     SELECT = enum.auto()
+    DOWNLOAD = enum.auto()
 
 
 class StateData(abc.ABC):
@@ -47,6 +54,16 @@ class SelectStateData(StateData):
         self.msg_id = msg_id
         self.filename = filename
         self.devices = devices
+
+
+class DownloadStateData(StateData):
+    def get_associated_state(self) -> States:
+        return States.DOWNLOAD
+
+    def __init__(self, msg_id: int, url: str, task: asyncio.Task):
+        self.msg_id = msg_id
+        self.url = url
+        self.task = task
 
 
 class OnStreamClosedHandler(OnStreamClosed):
@@ -123,12 +140,14 @@ class Bot:
 
     def prepare(self):
         admin_filter = filters.chat(self._config.admins) & filters.private
-        self._mtproto.register(MessageHandler(self._new_document, filters.document & admin_filter))
+        state_filter = create(lambda _, __, m: self._state_machine.get_state(m)[0] == States.NOTHING)
+        self._mtproto.register(MessageHandler(self._new_document, filters.document & admin_filter & state_filter))
         self._mtproto.register(MessageHandler(self._new_document, filters.video & admin_filter))
         self._mtproto.register(MessageHandler(self._new_document, filters.audio & admin_filter))
         self._mtproto.register(MessageHandler(self._new_document, filters.animation & admin_filter))
         self._mtproto.register(MessageHandler(self._new_document, filters.voice & admin_filter))
         self._mtproto.register(MessageHandler(self._new_document, filters.video_note & admin_filter))
+        self._mtproto.register(MessageHandler(self._new_link, filters.text & admin_filter & state_filter))
 
         admin_filter_inline = create(lambda _, __, m: m.from_user.id in self._config.admins)
         self._mtproto.register(CallbackQueryHandler(self._device_player_function, admin_filter_inline))
@@ -236,7 +255,7 @@ class Bot:
         if timeout_context.expired:
             await reply("Timeout while communicate with the device")
 
-    async def _new_document(self, _: Client, message: Message):
+    async def _new_document(self, _: Client, message: Message, user_message = None):
         devices = []
 
         for finder in self._finders.get_finders(self._config):
@@ -250,7 +269,7 @@ class Bot:
                 filename = "None"
 
             self._state_machine.set_state(
-                message,
+                user_message or message,
                 States.SELECT,
                 SelectStateData(
                     message.id,
@@ -266,3 +285,43 @@ class Bot:
 
         else:
             await message.reply("Supported devices not found in the network", reply_markup=_REMOVE_KEYBOARD)
+
+    async def _download_url(self, client, message, url):
+        try:
+            with tempfile.TemporaryDirectory() as tmpdir:
+                if 'youtube' in url or 'youtu.be' in url:
+                    output_filename = os.path.join(tmpdir, "video1.mp4")
+                    process = await asyncio.create_subprocess_shell(f"youtube-dl -v -f mp4 -o {output_filename} '{url}'")
+                else:
+                    output_filename = os.path.join(tmpdir, "video1")
+                    process = await asyncio.create_subprocess_shell(f"you-get -O {output_filename} '{url}'")
+                    output_filename = output_filename + ".mp4"
+
+                await process.communicate()
+
+                file_stats = os.stat(output_filename)
+                await message.reply(f"Download completed. Uploading video (size={file_stats.st_size})")
+                reader = open(output_filename, mode='rb')
+                video_message = await message.reply_video(reader)
+
+            await self._new_document(client, video_message, user_message=message)
+        except Exception as e:
+            self._state_machine.set_state(message, States.NOTHING, False)
+            await message.reply(f"Exception thrown {e} when downloading {url}: {traceback.format_exc()}")
+
+    async def _new_link(self, _: Client, message: Message):
+        state, state_data = self._state_machine.get_state(message)
+        if state == States.DOWNLOAD:
+            return await message.reply("Still downloading link", reply_markup=_REMOVE_KEYBOARD)
+
+        text = message.text.strip()
+
+        result = re.search(_URL_PATTERN, text)
+        if not result:
+            return await message.reply("Not a supported link", reply_markup=_REMOVE_KEYBOARD)
+
+        url = result.group(0)
+        task = asyncio.create_task(self._download_url(_, message, url))
+        self._state_machine.set_state(message, States.DOWNLOAD, DownloadStateData(message.id, url, task))
+        await message.reply(f"Downloading url {url}", reply_markup=_REMOVE_KEYBOARD, disable_web_page_preview=True)
+
