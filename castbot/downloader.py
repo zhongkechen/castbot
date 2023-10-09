da59f3966e36f3dbf307a7d268828c5d4e2b5177
Zhongke Chen <ch3n2k@gmail.com>
2023-10-08 23:14:01 -0700

    replace youtube-dl with yt-dlp

--- castbot/downloader.py
+++ castbot/downloader.py
@@ -7,7 +7,7 @@ import tempfile
 class Downloader:
     def __init__(self, config):
         self._downloader = config
-        assert self._downloader in ["youtube-dl", "you-get"]
+        assert self._downloader in ["yt-dlp", "you-get"]
 
     @contextlib.asynccontextmanager
     async def download(self, url):
@@ -15,7 +15,7 @@ class Downloader:
             if self._downloader == "youtube-dl":
                 output_filename = os.path.join(tmpdir, "video1.mp4")
                 process = await asyncio.create_subprocess_shell(
-                    f"youtube-dl -v -f mp4 -o {output_filename} '{url}'")
+                    f"yt-dlp -v -f mp4 -o {output_filename} '{url}'")
             else:  # "you-get"
                 output_filename = os.path.join(tmpdir, "video1")
                 process = await asyncio.create_subprocess_shell(f"you-get -O {output_filename} '{url}'")
