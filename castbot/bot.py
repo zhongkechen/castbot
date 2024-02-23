0fab0a74196202235995ebed0cbe5aa22f18ea7a
Zhongke Chen <ch3n2k@gmail.com>
2024-02-22 22:17:55 -0800

    move Telegram client code out of Bot class

--- castbot/bot.py
+++ castbot/bot.py
@@ -202,13 +202,14 @@ def pyrogram_filename(message: Message) -> str:
         raise TypeError() from error
 
 
-class Bot(BotInterface):
-    def __init__(self, config, downloader, http: Http, finders: DeviceFinderCollection):
+class BotClient(BotInterface):
+    def __init__(self, config):
         self._session_name = str(config["session_name"])
         self._api_id = int(config["api_id"])
         self._api_hash = str(config["api_hash"])
         self._token = str(config["token"])
         self._file_fake_fw_wait = float(config.get("file_fake_fw_wait", 0.2))
+
         self._client = Client(
             self._session_name,
             self._api_id,
@@ -218,6 +219,115 @@ class Bot(BotInterface):
             workdir=os.getcwd(),
         )
 
+    def register(self, handler: Handler):
+        self._client.add_handler(handler)
+
+    async def reply_message(self, message_id: int, chat_id: int, text: str):
+        await self._client.send_message(
+            chat_id,
+            text=text,
+            parse_mode=ParseMode.HTML,
+            reply_to_message_id=message_id,
+        )
+
+    @alru_cache
+    async def get_message(self, message_id: int) -> Message:
+        messages = await self._client.invoke(GetMessages(id=[InputMessageID(id=message_id)]))
+
+        if not messages.messages:
+            raise ValueError("wrong message_id")
+
+        message = messages.messages[0]
+
+        if not isinstance(message, RawMessage):
+            raise ValueError(f"expected `Message`, found: `{type(message).__name__}`")
+
+        return message
+
+    async def health_check(self):
+        if not all(x.is_started.is_set() for x in self._client.media_sessions.values()):
+            logging.log(logging.ERROR, "media session not connected")
+            raise ConnectionError()
+
+        if not self._client.session.is_started.is_set():
+            logging.log(logging.ERROR, "main session not connected")
+            raise ConnectionError()
+
+    async def get_block(self, message: RawMessage, offset: int, block_size: int) -> bytes:
+        session = self._client.media_sessions.get(message.media.document.dc_id)
+
+        request = GetFile(
+            offset=offset,
+            limit=block_size,
+            location=InputDocumentFileLocation(
+                id=message.media.document.id,
+                access_hash=message.media.document.access_hash,
+                file_reference=b"",
+                thumb_size="",
+            ),
+        )
+
+        result: typing.Optional[File] = None
+
+        while not isinstance(result, File):
+            try:
+                result = await session.invoke(request, sleep_threshold=0)
+            except FloodWait:  # file floodwait is fake
+                await asyncio.sleep(self._file_fake_fw_wait)
+
+        return result.bytes
+
+    async def start(self):
+        await self._client.start()
+
+        config = await self._client.invoke(GetConfig())
+        dc_ids = [x.id for x in config.dc_options]
+        keys_path = self._session_name + ".keys"
+
+        if os.path.exists(keys_path):
+            keys = pickle.load(open(keys_path, "rb"))
+        else:
+            keys = {}
+
+        for dc_id in dc_ids:
+            session = functools.partial(
+                Session,
+                self._client,
+                dc_id,
+                is_media=True,
+                test_mode=False,
+            )
+
+            if dc_id != await self._client.storage.dc_id():
+                if dc_id not in keys:
+                    exported_auth = await self._client.invoke(ExportAuthorization(dc_id=dc_id))
+
+                    auth = Auth(self._client, dc_id, False)
+                    auth_key = await auth.create()
+
+                    session = session(auth_key)
+                    await session.start()
+
+                    await session.invoke(ImportAuthorization(id=exported_auth.id, bytes=exported_auth.bytes))
+                    keys[dc_id] = session.auth_key
+
+                else:
+                    session = session(keys[dc_id])
+                    await session.start()
+
+            else:
+                session = session(await self._client.storage.auth_key())
+                await session.start()
+
+            self._client.media_sessions[dc_id] = session
+
+        pickle.dump(keys, open(keys_path, "wb"))
+        logging.info("Telegram server connected")
+
+
+class Bot(BotClient):
+    def __init__(self, config, downloader, http: Http, finders: DeviceFinderCollection):
+        super().__init__(config)
         self._downloader = downloader
         self._admins = config["admins"]
         if not isinstance(self._admins, list):
@@ -399,108 +509,3 @@ class Bot(BotInterface):
             await playing_video.send_stopped_control_message(remaining=remains)
             del self._playing_videos[local_token]
             await device.on_close(local_token)
-
-    def register(self, handler: Handler):
-        self._client.add_handler(handler)
-
-    async def reply_message(self, message_id: int, chat_id: int, text: str):
-        await self._client.send_message(
-            chat_id,
-            text=text,
-            parse_mode=ParseMode.HTML,
-            reply_to_message_id=message_id,
-        )
-
-    @alru_cache
-    async def get_message(self, message_id: int) -> Message:
-        messages = await self._client.invoke(GetMessages(id=[InputMessageID(id=message_id)]))
-
-        if not messages.messages:
-            raise ValueError("wrong message_id")
-
-        message = messages.messages[0]
-
-        if not isinstance(message, RawMessage):
-            raise ValueError(f"expected `Message`, found: `{type(message).__name__}`")
-
-        return message
-
-    async def health_check(self):
-        if not all(x.is_started.is_set() for x in self._client.media_sessions.values()):
-            logging.log(logging.ERROR, "media session not connected")
-            raise ConnectionError()
-
-        if not self._client.session.is_started.is_set():
-            logging.log(logging.ERROR, "main session not connected")
-            raise ConnectionError()
-
-    async def get_block(self, message: RawMessage, offset: int, block_size: int) -> bytes:
-        session = self._client.media_sessions.get(message.media.document.dc_id)
-
-        request = GetFile(
-            offset=offset,
-            limit=block_size,
-            location=InputDocumentFileLocation(
-                id=message.media.document.id,
-                access_hash=message.media.document.access_hash,
-                file_reference=b"",
-                thumb_size="",
-            ),
-        )
-
-        result: typing.Optional[File] = None
-
-        while not isinstance(result, File):
-            try:
-                result = await session.invoke(request, sleep_threshold=0)
-            except FloodWait:  # file floodwait is fake
-                await asyncio.sleep(self._file_fake_fw_wait)
-
-        return result.bytes
-
-    async def start(self):
-        await self._client.start()
-
-        config = await self._client.invoke(GetConfig())
-        dc_ids = [x.id for x in config.dc_options]
-        keys_path = self._session_name + ".keys"
-
-        if os.path.exists(keys_path):
-            keys = pickle.load(open(keys_path, "rb"))
-        else:
-            keys = {}
-
-        for dc_id in dc_ids:
-            session = functools.partial(
-                Session,
-                self._client,
-                dc_id,
-                is_media=True,
-                test_mode=False,
-            )
-
-            if dc_id != await self._client.storage.dc_id():
-                if dc_id not in keys:
-                    exported_auth = await self._client.invoke(ExportAuthorization(dc_id=dc_id))
-
-                    auth = Auth(self._client, dc_id, False)
-                    auth_key = await auth.create()
-
-                    session = session(auth_key)
-                    await session.start()
-
-                    await session.invoke(ImportAuthorization(id=exported_auth.id, bytes=exported_auth.bytes))
-                    keys[dc_id] = session.auth_key
-
-                else:
-                    session = session(keys[dc_id])
-                    await session.start()
-
-            else:
-                session = session(await self._client.storage.auth_key())
-                await session.start()
-
-            self._client.media_sessions[dc_id] = session
-
-        pickle.dump(keys, open(keys_path, "wb"))
-        logging.info("Telegram server connected")
